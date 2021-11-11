6832fb02106f7c67e3685a438bf10950dd80d7d7
namhd1992 <stpnghd.vn@gmail.com>
2021-11-11 11:19:18 +0700

    fix

--- src/containers/penalty/web/game/duatop/game.js
+++ src/containers/penalty/web/game/duatop/game.js
@@ -1,11 +1,14 @@
 import Phaser from "phaser";
 
-import ball_rotation from '../assert/ball_rotation.png';
+import ball_rotation from '../../../assert/ball.png';
+
+import k_idle from '../../../assert/k_idle.png';
 
 export default class Game extends Phaser.Scene{
     constructor() {
         super({ key: "Game" });
     }
+
 
     init(data){
         console.log('init', data);
@@ -14,7 +17,9 @@ export default class Game extends Phaser.Scene{
 
     
     preload(){
-        this.load.spritesheet('ball_rotation',ball_rotation, { frameWidth: 108, frameHeight: 108, endFrame: 23 });
+        this.add.
+        this.load.spritesheet('ball_rotation',ball_rotation, { frameWidth: 80, frameHeight: 80, endFrame: 23 });
+        this.load.spritesheet('k_idle',k_idle, { frameWidth: 140, frameHeight: 281, endFrame: 35 });
         
     }
 
@@ -61,13 +66,22 @@ export default class Game extends Phaser.Scene{
         var config = {
             key: 'explodeAnimation',
             frames: this.anims.generateFrameNumbers('ball_rotation', { start: 0, end: 23, first: 23 }),
-            frameRate: 20,
-            repeat: 1
+            frameRate: 6,
+            repeat: -1
+        };
+
+        var k_i = {
+            key: 'k_id',
+            frames: this.anims.generateFrameNumbers('k_idle', { start: 0, end: 35, first: 35 }),
+            frameRate: 6,
+            repeat: -1
         };
     
         this.anims.create(config);
+        this.anims.create(k_i);
     
         this.add.sprite(400, 300, 'ball_rotation').play('explodeAnimation');
+        this.add.sprite(400, 100, 'k_idle').play('k_id');
     }
 
     update(){
